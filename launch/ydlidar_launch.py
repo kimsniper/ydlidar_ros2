5213ea89fec87cc1eaf5d501a0dfa44e98766c99
kimsniper <kim1991mail@gmail.com>
2026-07-13 21:44:37 +0800

    fix

--- launch/ydlidar_launch.py
+++ launch/ydlidar_launch.py
@@ -15,39 +15,48 @@
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch_ros.actions import LifecycleNode
-from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-from launch.actions import LogInfo
+from launch_ros.actions import Node
 
-import lifecycle_msgs.msg
 import os
 
 
 def generate_launch_description():
     share_dir = get_package_share_directory('ydlidar')
     parameter_file = LaunchConfiguration('params_file')
-    node_name = 'ydlidar_node'
 
-    params_declare = DeclareLaunchArgument('params_file',
-                                           default_value=os.path.join(
-                                               share_dir, 'params', 'ydlidar.yaml'),
-                                           description='FPath to the ROS2 parameters file to use.')
+    params_declare = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(
+            share_dir,
+            'params',
+            'ydlidar.yaml'
+        ),
+        description='Path to the ROS2 parameters file to use.'
+    )
 
-    driver_node = LifecycleNode(package='ydlidar',
-                                node_executable='ydlidar_node',
-                                node_name='ydlidar_node',
-                                output='screen',
-                                emulate_tty=True,
-                                parameters=[parameter_file],
-                                node_namespace='/',
-                                )
-    tf2_node = Node(package='tf2_ros',
-                    node_executable='static_transform_publisher',
-                    node_name='static_tf_pub_laser',
-                    arguments=['0', '0', '0.02','0', '0', '0', '1','base_link','laser_frame'],
-                    )
+    driver_node = Node(
+        package='ydlidar',
+        executable='ydlidar_node',
+        name='ydlidar_node',
+        namespace='',
+        output='screen',
+        emulate_tty=True,
+        parameters=[parameter_file],
+    )
+
+    tf2_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_tf_pub_laser',
+        arguments=[
+            '0', '0', '0.02',
+            '0', '0', '0', '1',
+            'base_link', 'laser_frame'
+        ],
+        output='screen',
+    )
 
     return LaunchDescription([
         params_declare,
--- src/ydlidar_node.cpp
+++ src/ydlidar_node.cpp
@@ -75,49 +75,49 @@ int main(int argc, char *argv[]) {
   bool m_isToFLidar = false;
   bool m_Inverted = true;
 
-  node->declare_parameter("port");
+  node->declare_parameter("port", port);
   node->get_parameter("port", port);
 
-  node->declare_parameter("frame_id");
+  node->declare_parameter("frame_id", frame_id);
   node->get_parameter("frame_id", frame_id);
 
-  node->declare_parameter("ignore_array");
+  node->declare_parameter("ignore_array", list);
   node->get_parameter("ignore_array", list);
 
-  node->declare_parameter("baudrate");
+  node->declare_parameter("baudrate", baudrate);
   node->get_parameter("baudrate", baudrate);
 
-  node->declare_parameter("samp_rate");
+  node->declare_parameter("samp_rate", samp_rate);
   node->get_parameter("samp_rate", samp_rate);
 
-  node->declare_parameter("resolution_fixed");
+  node->declare_parameter("resolution_fixed", resolution_fixed);
   node->get_parameter("resolution_fixed", resolution_fixed);
 
-  node->declare_parameter("singleChannel");
+  node->declare_parameter("singleChannel", m_singleChannel);
   node->get_parameter("singleChannel", m_singleChannel);
 
-  node->declare_parameter("auto_reconnect");
+  node->declare_parameter("auto_reconnect", auto_reconnect);
   node->get_parameter("auto_reconnect", auto_reconnect);
 
-  node->declare_parameter("reversion");
+  node->declare_parameter("reversion", reversion);
   node->get_parameter("reversion", reversion);
 
-  node->declare_parameter("isToFLidar");
+  node->declare_parameter("isToFLidar", m_isToFLidar);
   node->get_parameter("isToFLidar", m_isToFLidar);
 
-  node->declare_parameter("angle_max");
+  node->declare_parameter("angle_max", angle_max);
   node->get_parameter("angle_max", angle_max);
 
-  node->declare_parameter("angle_min");
+  node->declare_parameter("angle_min", angle_min);
   node->get_parameter("angle_min", angle_min);
 
-  node->declare_parameter("max_range");
+  node->declare_parameter("max_range", max_range);
   node->get_parameter("max_range", max_range);
 
-  node->declare_parameter("min_range");
+  node->declare_parameter("min_range", min_range);
   node->get_parameter("min_range", min_range);
 
-  node->declare_parameter("frequency");
+  node->declare_parameter("frequency", frequency);
   node->get_parameter("frequency", frequency);
 
 
